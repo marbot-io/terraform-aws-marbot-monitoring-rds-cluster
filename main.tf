580e3d706fab72bc0a9f0f49d7b8a7047bf03eb1
Richard Worwood <richard.worwood@valtech.com>
2021-04-07 08:54:30 +0100

    remove artifacts

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-locals {
-  rds_instance = element(split(".", module.rds-host.string), 0)
-}
-
 ##########################################################################
 #                                                                        #
 #                                 TOPIC                                  #
