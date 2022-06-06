40ef43b735441066701d24110ea2b53729303192
Kratik Jain <kratik1998@gmail.com>
2022-06-06 09:52:11 +0530

    Added resource(DDB table) for remote backend

---
+++ backend-state-resource/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-dynamodb-table"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@
 # make sure your S3 bucket and DynamoDB table exists
 
 #! You can refer to this terraform file to learn more about how to create S3 bucket and DynamoDB table for terraform remote backend
+#? https://github.com/k4kratik/scalable-loki-with-s3-dynamodb/tree/main/infra/0-remote-backend-for-terraform
 terraform {
   backend "s3" {
     bucket         = "terraform-storage-bucket-170545349006"
