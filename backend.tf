50d2223a9fbde9fb1e49dd22e404d98ed961bf91
Kratik Jain <kratik1998@gmail.com>
2022-06-06 09:30:47 +0530

    Added comment for terraform backend

--- backend.tf
+++ backend.tf
@@ -1,5 +1,7 @@
 # make sure to replace the value with your own values.
 # make sure your S3 bucket and DynamoDB table exists
+
+#! You can refer to this terraform file to learn more about how to create S3 bucket and DynamoDB table for terraform remote backend
 terraform {
   backend "s3" {
     bucket         = "terraform-storage-bucket-170545349006"
