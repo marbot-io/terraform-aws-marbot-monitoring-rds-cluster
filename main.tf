106900737fb69c56fb3e25de39a4e6615c89486d
Michael Wittig <michael@widdix.de>
2023-04-28 11:07:25 +0200

    feat: add module_version_monitoring_enabled var

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ JSON
 
 resource "aws_cloudwatch_event_rule" "monitoring_jump_start_connection" {
   depends_on = [aws_sns_topic_subscription.marbot]
-  count      = var.enabled ? 1 : 0
+  count      = (var.module_version_monitoring_enabled && var.enabled) ? 1 : 0
 
   name                = "marbot-rds-cluster-connection-${random_id.id8.hex}"
   description         = "Monitoring Jump Start connection. (created by marbot)"
@@ -108,7 +108,7 @@ resource "aws_cloudwatch_event_rule" "monitoring_jump_start_connection" {
 }
 
 resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
-  count = var.enabled ? 1 : 0
+  count = (var.module_version_monitoring_enabled && var.enabled) ? 1 : 0
 
   rule      = join("", aws_cloudwatch_event_rule.monitoring_jump_start_connection.*.name)
   target_id = "marbot"
@@ -117,7 +117,7 @@ resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
 {
   "Type": "monitoring-jump-start-tf-connection",
   "Module": "rds-cluster",
-  "Version": "0.9.0",
+  "Version": "0.10.0",
   "Partition": "${data.aws_partition.current.partition}",
   "AccountId": "${data.aws_caller_identity.current.account_id}",
   "Region": "${data.aws_region.current.name}"
