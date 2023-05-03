34aea37befd2500cfeb99ddc7f292511880f9c2c
Michael Wittig <michael@widdix.de>
2023-05-03 09:30:59 +0200

    feat: support config via tags

--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+# We can not only check the var.topic_arn !="" because of the Terraform error:  The "count" value depends on resource attributes that cannot be determined until apply, so Terraform cannot predict how many instances will be created.
+variable "create_topic" {
+  type        = bool
+  description = "Create SNS topic? If set to false you must set topic_arn as well!"
+  default     = true
+}
+
+variable "topic_arn" {
+  type        = string
+  description = "Optional SNS topic ARN if create_topic := false (usually the output of the modules marbot-monitoring-basic or marbot-standalone-topic)."
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "marbot stage (never change this!)."
+  default     = "v1"
+}
+
 variable "endpoint_id" {
   type        = string
   description = "Your marbot endpoint ID (to get this value: select a channel where marbot belongs to and send a message like this: \"@marbot show me my endpoint id\")."
@@ -26,45 +45,81 @@ variable "db_cluster_identifier" {
   description = "The cluster identifier of the RDS Aurora cluster that you want to monitor."
 }
 
+
+
+variable "cpu_utilization" {
+  type        = string
+  description = "CPU utilization (static|off)."
+  default     = "static"
+}
+
 variable "cpu_utilization_threshold" {
   type        = number
-  description = "The maximum percentage of CPU utilization (set to -1 to disable)."
+  description = "The maximum percentage of CPU utilization (0-100)."
   default     = 80
 }
 
-variable "burst_monitoring_enabled" {
-  type        = bool
-  description = "Deprecated, set variable cpu_credit_balance_threshold to -1 instead"
-  default     = true
+variable "cpu_utilization_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
+}
+
+variable "cpu_utilization_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
+}
+
+
+
+
+variable "cpu_credit_balance" {
+  type        = string
+  description = "CPU burst credits for t* instances (static|off)."
+  default     = "static"
 }
 
 variable "cpu_credit_balance_threshold" {
   type        = number
-  description = "The minimum number of CPU credits available (t* instances only; set to -1 to disable)."
+  description = "The minimum number of CPU credits remaining in the burst bucket (>= 0)."
   default     = 20
+}
+
+variable "cpu_credit_balance_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
+}
+
+variable "cpu_credit_balance_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
+}
+
+
+
+variable "freeable_memory" {
+  type        = string
+  description = "Available memory (static|off)."
+  default     = "static"
 }
 
 variable "freeable_memory_threshold" {
   type        = number
-  description = "The minimum amount of available random access memory in Byte (set to -1 to disable)."
+  description = "The minimum amount of available random access memory in Byte (>= 0)."
   default     = 64000000 # 64 Megabyte in Byte
 }
 
-# We can not only check the var.topic_arn !="" because of the Terraform error:  The "count" value depends on resource attributes that cannot be determined until apply, so Terraform cannot predict how many instances will be created.
-variable "create_topic" {
-  type        = bool
-  description = "Create SNS topic? If set to false you must set topic_arn as well!"
-  default     = true
+variable "freeable_memory_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
 }
 
-variable "topic_arn" {
-  type        = string
-  description = "Optional SNS topic ARN if create_topic := false (usually the output of the modules marbot-monitoring-basic or marbot-standalone-topic)."
-  default     = ""
-}
-
-variable "stage" {
-  type        = string
-  description = "marbot stage (never change this!)."
-  default     = "v1"
+variable "freeable_memory_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
 }
